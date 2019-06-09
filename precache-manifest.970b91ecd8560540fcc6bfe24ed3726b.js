8c74cc3cf3bee82e228c4f3f0595e62c5cc240ff
Aleksei Privezentcev <a.privezentcev@gmail.com>
2019-06-09 15:07:51 +0400

    Updates

---
+++ precache-manifest.970b91ecd8560540fcc6bfe24ed3726b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d855486bf40287934e0e6e19b3edf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3320a17cb259ab31b9d3",
+    "url": "/static/css/main.04c955c8.chunk.css"
+  },
+  {
+    "revision": "24a716a3ffd9a7b03b1e",
+    "url": "/static/js/2.01b1dc22.chunk.js"
+  },
+  {
+    "revision": "3320a17cb259ab31b9d3",
+    "url": "/static/js/main.c94d03ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
